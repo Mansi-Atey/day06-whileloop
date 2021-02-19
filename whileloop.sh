66a96a01d61edbe7e0241c36945edb46f4c812df
unknown <you@example.com>
2021-02-20 00:56:21 +0530

    Addded: code of magic number

--- whileloop.sh
+++ whileloop.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
- echo "Enter n value"
+  echo "keep any number between 1 to 0 in mind" 
   read n
-i=1
- while [ $i -le $n ]
-do
- num=1
- j=1
-whilw [ $j -le $i ]
-do
-  num=$(($num * 2))
-  j=$(($j+1))
-done
-echo "answer is" $num
-i=$(($i+1))
+start=1
+end=10
+yes=1
+no=0
+mid2=0
+ while [ $mid2 -ne $n ]
+ do
+ mid1=$(($start+$end))
+ mid2=$(($mid1/2))
+echo "is this your number $mid2:-"
+read answer
+ if [ $answer -eq $yes ]
+ then
+    echo "Number found"
+ else
+    echo "is your number greater or smaller..?"
+  read answer1
+if [ $answer1 -eq $yes ]
+ then
+   start=$mid2
+ else
+  end=$mid2
+  fi
+ fi
 done
