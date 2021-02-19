9a8586bdbb6de7dbc766ab493367441dce7d6cba
unknown <you@example.com>
2021-02-20 01:00:33 +0530

    Addded: code for head or tail 11 times

--- whileloop.sh
+++ whileloop.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
-  echo "keep any number between 1 to 0 in mind" 
-  read n
-start=1
-end=10
-yes=1
-no=0
-mid2=0
- while [ $mid2 -ne $n ]
- do
- mid1=$(($start+$end))
- mid2=$(($mid1/2))
-echo "is this your number $mid2:-"
-read answer
- if [ $answer -eq $yes ]
- then
-    echo "Number found"
- else
-    echo "is your number greater or smaller..?"
-  read answer1
-if [ $answer1 -eq $yes ]
- then
-   start=$mid2
- else
-  end=$mid2
-  fi
+ tail=0
+head=1
+cHeads=0
+cTails=0
+while [ $cHeads -lt "11" ]
+do
+ rndNum=$(($RANDOM%2))
+if [ $rndNum -eq "1" ]
+then
+ cHeads=$(($cHeads+1))
+echo "Heads win" $cHeads
+else
+ echo "Tails win" 
  fi
 done
