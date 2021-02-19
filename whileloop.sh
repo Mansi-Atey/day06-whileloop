607f6a83bf166db8e29e3be5a382fd1a3de529e5
unknown <you@example.com>
2021-02-20 01:04:57 +0530

    Addded: code of gamble

--- whileloop.sh
+++ whileloop.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
- tail=0
-head=1
-cHeads=0
-cTails=0
-while [ $cHeads -lt "11" ]
-do
- rndNum=$(($RANDOM%2))
-if [ $rndNum -eq "1" ]
-then
- cHeads=$(($cHeads+1))
-echo "Heads win" $cHeads
+cash=100
+goal=200
+bet=20
+loose=0
+won=1
+while [ $cash -gt $bet && $cash -lt $goal ]
+d0
+   randomNumber=$(($RANDOM%2))
+     if [ $randomNumber -eq $loose ]
+     then
+     cash=(($cash - $bet * 2))
 else
- echo "Tails win" 
- fi
+ cash=(($cash + $bet * 2))
+fi
 done
+if [$cash -ge $goal]
+ then
+ echo "Player Won"
+else
+ echo "Player loose"
+fi
